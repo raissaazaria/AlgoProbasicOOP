ed87bde8390bb5ef65dd6649115d4a42ca6e19c1
raissaazaria <91534032+raissaazaria@users.noreply.github.com>
2021-12-08 23:35:50 +0700

    Update DriverRA.py

--- DriverRA.py
+++ DriverRA.py
@@ -1,22 +1,22 @@
-from shoppingListRA import definitionModule
+from shoppingListRA import definitionModule #importing the class module to this file
 
-def listofObject():
-    li = []
-    numberItems = 0
-    while numberItems < 1:
-        numberItems = int(input("How many items will you order today?"))
-        print("The number of items purchases is at least 1")
+def listofObject(): #function to create new list
+    li = [] #empty list
+    numberItems = 0 #used as variable in looping
+    while numberItems < 1: #input the rule that number must be greater than 1
+        numberItems = int(input("How many items will you order today?")) 
+        print("The number of items purchases is at least 1") #print this if less than 1
     for i in range(numberItems):
         print(f"Item #{i+1}")
-        food= input("Enter food name:")
+        name= input("Enter food name:")
         amount= input("Enter amount of pounds:")
-        while amount <= 0:
+        while amount <= 0: #amount must be greater than 0
             print("Amounts of pounds must be greater than 0.")
-            amount = ("Enter amounts of pounds:")
-        li.append(definitionModule(food,amount))
+            amount = ("Enter amounts of pounds:") #re write available number
+        li.append(definitionModule(name,amount,price,calculatedPrice))
     return li
 
-def displayContent(list):
+def displayContent(list): #print every information that has been updated
          print("Here's a summary of the items purchased:")
          for displayContent in list:
              print(f"Items:{displayContent.getName()}")
@@ -24,13 +24,13 @@ def displayContent(list):
              print(f"Price per pound: ${displayContent.getPrice():.2f}")
              print(f"price of order: ${displayContent.getcalculatedPrice():.2f}")
 
-def total(list):
+def total(list): #count the total using formula
     total = 0
     for i in list:
         total = i.getcalculatedPrice + total
         print("Your total payment are: ${:.2f}".format(total))
 
-def main():
+def main():# print
     list = listofObject()
     displayContent(list)
     total(list)
